9a3db1d4520700f059051965aa93669548146680
Team <team@fastcode.com.br>
2022-03-23 18:12:07 -0300

    {WIP} Continents Section

--- src/components/Home/ContinentsSection/index.tsx
+++ src/components/Home/ContinentsSection/index.tsx
@@ -9,7 +9,9 @@ export const ContinentsSection = () => {
       <div className={styles.linearGradientBg} /> {/* Linear Gradient Background */}
 
       <div className={styles.continentsSectionContainer}>
-
+        <span>
+          Presente em quase todos<br />os <strong>continentes do mundo</strong>
+        </span>
       </div>
 
       <div className={styles.imageContainer}>
