5ab61498c2e053fa5007eabb4883f38b14bd801f
Team <team@fastcode.com.br>
2022-04-04 17:08:39 -0300

    Denise Mobile Img and QA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { SegmentsSection } from '../components/Home/SegmentsSection';
 import { TryoutSection } from '../components/Home/TryoutSection';
 import { ContinentsSection } from '../components/Home/ContinentsSection';
 import deniseImg from '../../public/images/denise.png';
+import deniseMobileImg from '../../public/images/deniseMobile.jpg';
 
 import styles from './home.module.scss';
 import { DifferentialsSection } from '../components/Home/DifferentialsSection';
@@ -23,7 +24,7 @@ export default function Home() {
 
       <div className={styles.imageContainer}>
         <Image
-          src={deniseImg}
+          src={width > 768 ? deniseImg : deniseMobileImg}
           objectFit="cover"
           alt="Foto de Denise com diagnóstico"
         />
