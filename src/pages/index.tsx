3d30ae0dfa4283e2fe74c3645363de488a563670
Team <team@fastcode.com.br>
2022-04-13 14:51:35 -0300

    MetaTags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ const Home: NextPage = () => {
         title="Home | Paciente 360"
         description="A única plataforma de simulação de casos clínicos com pessoas reais no mundo.
         Utilizamos o ambiente digital para oferecer uma ferramenta que amplia o acesso à educação em saúde, de forma prática, interativa e de forma humanizada, com interações da experiência real."
-        image="../../public/images/hero-mobile.png"
+        image="/public/images/hero-mobile.png"
       />
 
       <div className={styles.imageContainer}>
