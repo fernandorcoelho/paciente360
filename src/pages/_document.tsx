71584f82fc48e04c782b528617cf26a5ad004244
Team <team@fastcode.com.br>
2022-05-02 15:11:15 -0300

    {QA} google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -27,12 +27,15 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           {/* Google Tag Manager */}
           <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-127061973-1%22%3E"
+          />
+          <script
             dangerouslySetInnerHTML={{
-              __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-MTGHZT9');`
+              __html: `(window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-127061973-1'));`
             }}
           />
           {/* End Google Tag Manager */}
